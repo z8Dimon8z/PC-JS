e9193797721a2e50346721324deeb1408c2eb91f
Dima Lysak <60256445+z8Dimon8z@users.noreply.github.com>
2022-04-22 11:17:48 +0300

    Day 1

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// акардеон
+const contents = document.querySelectorAll('.program-line__content');
+contents.forEach(elem => {
+const title = elem.querySelector('.program-line__title');
+const descr = elem.querySelector('.program-line__descr');
+
+  title.addEventListener('click', ()=> {
+    if (!descr.classList.contains('active')){
+      descr.classList.toggle('active');
+    } else{
+      descr.classList.remove('active');
+    }
+
+  });
+
+});
+
