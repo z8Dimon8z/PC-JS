4b7832e8826afc61193889ffce1f183a5a9185dc
Dima Lysak <60256445+z8Dimon8z@users.noreply.github.com>
2022-04-24 20:50:05 +0300

    day-3 task-2

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,29 @@
 const modalBtn = document.querySelector('.modal__button');
 const modal = document.querySelector('.modal');
+const modalInner = document.querySelector('.modal__inner');
+
+modalInner.style.position = 'relative';
+
+const addCloseBtn = () => {
+  const closeBtn = document.createElement('div');
+
+  closeBtn.classList.add('close-button');
+  closeBtn.textContent = 'X';
+  closeBtn.style.position = 'absolute';
+  closeBtn.style.right = '10px';
+  closeBtn.style.top = '10px';
+  closeBtn.style.padding = '3px';
+  closeBtn.style.width = '20px';
+  closeBtn.style.height = '20px';
+  closeBtn.style.fontSize = '20px';
+  closeBtn.style.cursor = 'pointer';
+
+  modalInner.append(closeBtn);
+
+  closeBtn.addEventListener('click', ()=>{
+  modal.style.display = 'none';
+});
+}
 
 modalBtn.addEventListener('click', ()=>{
   modal.style.display = 'flex';
@@ -11,6 +35,8 @@ modal.addEventListener('click', (event)=>{
     modal.style.display = 'none';
   }
 });
+
+addCloseBtn();
 
 
 
